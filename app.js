2fe7a3a60a67589794caf28253d02e1d12897cda
Anar <anarq2006q@proton.me>
2025-08-17 16:54:33 +0000

    ready

--- app.js
+++ app.js
@@ -1,4 +1,55 @@
 "use strict";
 $(document).ready(function () {
 
+
+
+
+    $(document).on("keypress", function (e) {
+        if (e.which == 13) {
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#mycity").val()}&appid=d93403b35da76cc8c819933139eeaafd&units=metric`
+
+            async function myweather() {
+                let responce = await fetch(url);
+                let data = await responce.json()
+
+                $("#city").html(data.name);
+                $("#temperature").html(data.main.temp);
+                $("#feelslike").html(data.main.feels_like);
+                $("#mycity").val("")
+                console.log(data);
+            }
+            myweather();
+        }
+    });
+
+
+    $(".btn").click(function (e) {
+        e.preventDefault();
+
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$('#mycity').val()}&appid=d93403b35da76cc8c819933139eeaafd&units=metric`;
+
+        async function myweather() {
+            let responce = await fetch(url);
+            let data = await responce.json();
+            $("#city").html(data.name);
+            $("#temperature").html(data.main.temp);
+            $("#feelslike").html(data.main.feels_like);
+            $("#mycity").val("")
+            console.log(data);
+        }
+        myweather();
+
+    });
+
+
+
+
+
+
+
+
+
+
 });
+
+
